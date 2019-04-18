06971ff1639400b7a9bfe0dd18c0bc973d6d4f64
VISHAL GUPTA <49202726+vishalgupta07@users.noreply.github.com>
2019-04-18 13:45:46 +0530

    Update nms_demo.py

--- tensorflow-yolov3/nms_demo.py
+++ tensorflow-yolov3/nms_demo.py
@@ -1,16 +1,3 @@
-#! /usr/bin/env python
-# coding=utf-8
-#================================================================
-#   Copyright (C) 2018 * Ltd. All rights reserved.
-#
-#   Editor      : VIM
-#   File name   : nms_demo.py
-#   Author      : YunYang1994
-#   Created date: 2018-11-27 13:02:17
-#   Description :
-#
-#================================================================
-
 import time
 import numpy as np
 import tensorflow as tf
