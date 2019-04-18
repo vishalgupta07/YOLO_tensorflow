34bca60c1721382f47182bd0abd37db024e6ec12
VISHAL GUPTA <49202726+vishalgupta07@users.noreply.github.com>
2019-04-18 13:44:59 +0530

    Update quick_train.py

--- tensorflow-yolov3/quick_train.py
+++ tensorflow-yolov3/quick_train.py
@@ -1,16 +1,3 @@
-#! /usr/bin/env python
-# coding=utf-8
-#================================================================
-#   Copyright (C) 2019 * Ltd. All rights reserved.
-#
-#   Editor      : VIM
-#   File name   : quick_train.py
-#   Author      : YunYang1994
-#   Created date: 2019-01-21 14:46:26
-#   Description :
-#
-#================================================================
-
 import tensorflow as tf
 from core import utils, yolov3
 from core.dataset import dataset, Parser
