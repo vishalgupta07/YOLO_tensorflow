fc66ee8fb60069f0634eb4c0f735d8ea5dd40f2c
VISHAL GUPTA <49202726+vishalgupta07@users.noreply.github.com>
2019-04-18 13:47:36 +0530

    Update extract_voc.py

--- tensorflow-yolov3/scripts/extract_voc.py
+++ tensorflow-yolov3/scripts/extract_voc.py
@@ -1,17 +1,3 @@
-#! /usr/bin/env python
-# coding=utf-8
-#================================================================
-#   Copyright (C) 2019 * Ltd. All rights reserved.
-#
-#   Editor      : VIM
-#   File name   : extract_voc.py
-#   Author      : YunYang1994
-#   Created date: 2019-01-05 00:14:10
-#   Description :
-#
-#================================================================
-
-
 import os
 import argparse
 import xml.etree.ElementTree as ET
