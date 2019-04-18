b150a8c684d757a266f639957ff9b47f05a956ef
VISHAL GUPTA <49202726+vishalgupta07@users.noreply.github.com>
2019-04-18 13:46:09 +0530

    Update kmeans.py

--- tensorflow-yolov3/kmeans.py
+++ tensorflow-yolov3/kmeans.py
@@ -1,16 +1,3 @@
-#! /usr/bin/env python
-# coding=utf-8
-#================================================================
-#   Copyright (C) 2019 * Ltd. All rights reserved.
-#
-#   Editor      : VIM
-#   File name   : kmeans.py
-#   Author      : YunYang1994
-#   Created date: 2019-01-25 11:08:15
-#   Description :
-#
-#================================================================
-
 import cv2
 import argparse
 import numpy as np
